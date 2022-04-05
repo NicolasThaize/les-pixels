d6622243d9449a9933dd959a1c45210dc36b411e
NicolasThaize <nicolas.thaize@ynov.com>
2022-04-05 12:27:46 +0200

    Init front displays

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,20 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+app.set('views', __dirname + '/pages');
+app.set('view engine', 'ejs'); // Sets the view engine
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
+});
+
+app.get('/register',(req, res) => {
+    res.render('register/index.ejs');
+});
+
+app.get('/login',(req, res) => {
+    res.render('login/index.ejs');
 });
 
 app.listen(port, () => {
